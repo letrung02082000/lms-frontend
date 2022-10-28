492e569d4f54419c8496ced17cc5db4cdd65ee4e
Trung <letrung02082000@gmail.com>
2022-10-28 16:16:43 +0700

    Remove bicycle service

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'react-redux'
 import { store } from './store/index'
-
+import theme from './theme'
 import 'react-datetime/css/react-datetime.css'
 import 'react-toastify/dist/ReactToastify.css'
 import 'assets/styles/global.scss'
@@ -13,9 +14,12 @@ import { ModalProvider } from 'react-modal-hook'
 
 ReactDOM.render(
   <Provider store={store}>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 )
