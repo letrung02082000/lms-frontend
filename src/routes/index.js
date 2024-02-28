7d1ab833dfc73caa8a9eec9a68a97eb5e7acf647
Lê Văn Trung <49792818+letrung02082000@users.noreply.github.com>
2024-02-28 19:13:32 +0700

    remove guard

--- src/routes/index.js
+++ src/routes/index.js
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
     ],
   },
   {
-    element: <AdminDrivingGuard />,
+    // element: <AdminDrivingGuard />,
     errorElement: <NotFoundPage />,
     children: [
       {
